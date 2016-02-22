551401c90851aefc73f1c132bdc83d08167b5f59
fred vultor <fred.vultor@gmail.com>
2016-02-22 13:13:59 -0300

    tridentate done

--- src/Ligand.cpp
+++ src/Ligand.cpp
@@ -41,6 +41,7 @@ bool Ligand::initializeLigand()
 
 	case 3:
 		sucess2 = calculateTridentate();
+		break;
 
 	default:
 		sucess2 = false;
@@ -187,9 +188,66 @@ bool Ligand::calculateTridentate()
 		return false;
 	}
 
+	AuxMath AuxMath_;
+	vector<double> geometricCenter = AuxMath_.triangleCentroid(
+		coord[0].x, coord[0].y, coord[0].z,	
+		coord[1].x, coord[1].y, coord[1].z,
+		coord[2].x, coord[2].y, coord[2].z
+		);
+	X1.x = geometricCenter[0];
+	X1.y = geometricCenter[1];
+	X1.z = geometricCenter[2];
 
+	vector<double> centroid(3);
+	centroid[0] = 0.0e0;
+	centroid[1] = 0.0e0;
+	centroid[2] = 0.0e0;
+	for (size_t i = 0; i < coord.size(); i++)
+	{
+		centroid[0] += coord[i].x;
+		centroid[1] += coord[i].y;
+		centroid[2] += coord[i].z;
+	}
+	centroid[0] /= coord.size();
+	centroid[1] /= coord.size();
+	centroid[2] /= coord.size();
 
+	vector<double> normal = AuxMath_.normalVectorFrom3Points(
+		coord[0].x, coord[0].y, coord[0].z,
+		coord[1].x, coord[1].y, coord[1].z,
+		coord[2].x, coord[2].y, coord[2].z
+		);
+	X2.x = normal[0];
+	X2.y = normal[1];
+	X2.z = normal[2];
 
+	// if it points to centroid change direction
+	double angle = AuxMath_.angleFrom3Points(
+		X2.x + X1.x, X2.y + X1.y, X2.z + X1.z,
+		X1.x, X1.y, X1.z,
+		centroid[0], centroid[1], centroid[2]
+		);		
+
+	const double _pi = 3.14159265358979323846;
+
+	if (angle < _pi/2.0e0)
+	{
+		X2.x *= -1.0e0;
+		X2.y *= -1.0e0;
+		X2.z *= -1.0e0;
+	}
+
+#ifdef _DEBUG
+	X2.x += X1.x;
+	X2.y += X1.y;
+	X2.z += X1.z;
+	X1.atomlabel = "Au";
+	X2.atomlabel = "Cu";
+	coord.push_back(X1);
+	coord.push_back(X2);
+	printXyzLigandDirection();
+#endif
+	
 	return true;
 }
 

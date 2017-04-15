226dea67a3949b473b4e28cc8f74decfa0a21f5e
vultor <fred.vultor@gmail.com>
2017-04-15 20:57:38 -0300

    scrit

--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,8 @@ int main(int argc, char *argv[])
 	./lumpacview.exe generateCompositionFiles 6 30 12 3 m01m02m03m04m04m04 skeleton-6 pc
 	./lumpacview.exe cleanBlocks m01m02m03m04m04m04 6 3 pc
 	cat composition---atomTypes composition-independent* > results-composition
+
+	criar uma function que retorna o working directory
 */
 
 	stringstream convert0;

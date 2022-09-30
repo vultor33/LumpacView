feaf9722cabd38be1e3cba58741c428f7768e5b3
Frederico Teixeira <frederico@jettacargo.com.br>
2022-09-30 13:13:03 -0300

    feat: permutation to vultor task

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-    std::cout << "novo main" << std::endl;
+    std::cout << "stereoisomer started" << std::endl;
     PermutationToVultorCode permutation_to_vultor_code = PermutationToVultorCode();
     permutation_to_vultor_code.run();
     return 0;
--- src/tasks/PermutationToVultorCode.cpp
+++ src/tasks/PermutationToVultorCode.cpp
@@ -9,12 +9,42 @@ using namespace std;
 PermutationToVultorCode::PermutationToVultorCode(){}
 PermutationToVultorCode::~PermutationToVultorCode(){}
 
+/*
+INPUT INFORMATION NEEDED
+* geo_code (consult Geometris.cpp)
+* composition - e.x. Ma4(AB)
+* permutation - e.x. 0 2 3 1
+OUTPUT
+* point group (string)
+
+HOW TO RUN
+
+1. Transform composition to accepted format:
+   e.x. Maaabbc = Ma3b2c
+
+2. Create a file in ./data with the name:
+   "GEO-XX-" + your_composition + ".csv"
+   e.x. GEO-10-Maaaaaaaaaa.csv
+
+3. Put atom types in the first line. (start with 1)
+   e.x. 2 3 3 1 1 
+
+4. Put your permutation starting with 1,
+   following the template:
+   [DUM] [your_permutation_here] A {DUMMY}
+   e.x. [DUM] [1 2 3 4 5 6 7 8 9 10] A {DUMMY}
+
+5. Hard code this file for parameters
+
+6. Run this task. All your permutations will be written in
+   the .exe folder with the name input_name.
+*/
+
 void PermutationToVultorCode::run()
 {
-    std::cout << "Dentro do task" << std::endl;
-    int geo_code = 101;
-    string input_name = "GEO-10-Maaaaaaaaaa.csv";
-    string input_path = "/home/fred/pesquisa/LumpacView/data/";
+    int geo_code = 103; // PUT YOUR GEO CODE HERE
+    string input_name = "GEO-10-Maaaaaaaaaa.csv"; // PUT YOUR FILE NAME HERE
+    string input_path = "/home/fred/pesquisa/LumpacView/data/"; //PUT THE PATH OF YOUR DATA FOLDER HERE
 
 	CauchyIndex ciSymmetry_(geo_code);
 	ciSymmetry_.findAllSymmetryOperations(geo_code, input_name, input_path);

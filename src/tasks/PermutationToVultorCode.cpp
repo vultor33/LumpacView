5cbd615c7200c004fc4048abe2f9452451a3a9af
Frederico Teixeira <frederico@jettacargo.com.br>
2022-09-30 10:43:27 -0300

    refactor: individual tasks

---
+++ src/tasks/PermutationToVultorCode.cpp
@@ -0,0 +1,11 @@
+#include "PermutationToVultorCode.h"
+
+#include <iostream>
+
+PermutationToVultorCode::PermutationToVultorCode(){}
+PermutationToVultorCode::~PermutationToVultorCode(){}
+
+void PermutationToVultorCode::run()
+{
+    std::cout << "Dentro do task" << std::endl;
+}
---
+++ src/tasks/PermutationToVultorCode.h
@@ -0,0 +1,19 @@
+#ifndef PERMUTATION_TO_VULTOR_CODE_H
+#define PERMUTATION_TO_VULTOR_CODE_H
+
+#include <string>
+
+class PermutationToVultorCode
+{
+
+public:
+
+    PermutationToVultorCode();
+    ~PermutationToVultorCode();
+
+    void run();
+
+};
+
+
+#endif
